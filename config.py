cb9fc33885281a7404520b547c5caa7483643f8e
oxinabox <oxinabox@ucc.asn.au>
2015-02-09 22:44:11 +0800

    =worked on comamndlin

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 
-HOME = './test'
-CASTLE_PATH = HOME + '/dotfuss-castle'
+HOME = '/home/wheel/oxinabox/programming/dotfuss/test/'
+CASTLE_PATH = HOME + 'dotfuss-castle/'
 
 
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
--- dotfuss.py
+++ dotfuss.py
@@ -1,20 +1,41 @@
 import config
 
 import glob
-import os.path
+import os.path as path
 
-def link_status(rel_path, include".*", exclude="*.swp"):
-    def get_files(path):
-        included = glob.glob(os.path.join(path,include))
-        excluded = glob.glob(os.path.join(path,include))
-        return set(included) - set(excluded)
+def link_status(include=".*", exclude="*.swp"):
+    def get_files(fpath):
+        def get_interior_name(exterior_filename):
+            return exterior_filename[len(fpath):]
+
+        included = glob.glob(path.join(fpath,include))
+        excluded = glob.glob(path.join(fpath,exclude))
+        
+        files = set(included) - set(excluded)
+        return set(map(get_interior_name, files))
+    
 
     home_files   = get_files(config.HOME)
     castle_files = get_files(config.CASTLE_PATH)
+    
+    print("\nLink Status\n" + 10*"-")
+    print('\n')  
+   
+    print home_files
+    print castle_files
 
+    for filename in home_files.intersection(castle_files):
+        print filename
+        if path.samefile(path.join(config.CASTLE_PATH, filename),
+                         path.join(config.HOME, filename)):
+            print(':-)')
+            print config.bcolors.OKGREEN +  "[LINKED] " + filename
+        else: print config.bcolors.FAIL +  "[BORKED] " + filename
     
-    
-    
+    print('\n')   
+    for filename in home_files - castle_files:
+        print config.bcolors.WARNING +  "[UNCONTROLLED] "+filename
+ 
 
 if __name__ =="__main__":
-    list_all(config.home)
+    link_status()
