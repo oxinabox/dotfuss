dbb5be0b6888cfd50ee9b1a05e154f886d4cb6aa
oxinabox <oxinabox@ucc.asn.au>
2015-02-15 11:34:28 +0800

    =script to create a testing enviroment created

---
+++ make_test_enviroment.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+mkdir test
+cd test
+mkdir dotfuss-castle
+
+touch norm
+touch ordinary
+touch .eg
+touch .other
+touch .other.swp
+touch .specrc
+
+mkdir .config
+cd .config
+touch confeg
+touch .confconf
+
+cd ..
+mkdir normdir
+cd normdir
+touch normfile
+
+cd ../..
